acf7c15a63de5b2581d63c915a393616973972cd
Jen Thomas <jenny_t152@yahoo.co.uk>
2019-06-27 14:25:36 +0100

    Corrected typo

--- match_bottle_times_positions.sh
+++ match_bottle_times_positions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 INPUT_BOTTLE_FILE_PATH="$1"
 OUTPUT_BOTTLE_FILE_PATH="$2"
@@ -17,5 +17,5 @@ do
 
     output_matched_positions_filename="$bottle_file_basename_no_suffix-positions.csv"
 
-    ./get_positions.py "$bottles_with_date_time" "$LAT_LONG_INPUT_FILE_PATH" "$LAT_LONG_RESOLUTION" "$OUTPUT_MATCHED_POSITIONS_FILE_PATH" "$ouput_matched_positions_filename"
+    ./get_positions.py "$bottles_with_date_time" "$LAT_LONG_INPUT_FILE_PATH" "$LAT_LONG_RESOLUTION" "$OUTPUT_MATCHED_POSITIONS_FILE_PATH" "$output_matched_positions_filename"
 done
