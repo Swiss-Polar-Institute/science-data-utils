cf9c80e3e649eac224f28403199122df38f7a880
Jen Thomas <jenny_t152@yahoo.co.uk>
2019-09-23 15:05:48 +0100

    Added further comments to understand the pump log code

--- ferrybox_bad_data_times_to_status.py
+++ ferrybox_bad_data_times_to_status.py
@@ -18,33 +18,39 @@ def process_file(input_file):
     # sort the list of data so that the missing times can be added one line at a time
     data.sort()
 
-    # get the first row of data
+    # get the first row of data and create the first "step" out of it
     first_row = data[0]
 
     first_time_off = datetime.datetime.strptime(first_row[0] + " " + first_row[1], "%Y-%m-%d %H:%M:%S")
     first_time_on = datetime.datetime.strptime(first_row[0] + " " + first_row[2], "%Y-%m-%d %H:%M:%S")
 
-    #print(first_time_off)
-    #print(first_time_on)
-
-    first_step = [first_time_off, first_time_on, 'off']
-
-    previous_time_on = first_time_on
-
+    # each step will be defined in a list: start time, end time, status
     previous_step = []
     next_step = []
 
+    # this first row has already been designated as off
+    first_step = [first_time_off, first_time_on, 'off']
+
+    # set the time the pump was on for the next row, as the time from the first row
+    previous_time_on = first_time_on
+
+    # create an iterator to avoid having to read the first row of the list (this has already been used)
     data_iter = iter(data)
 
     next(data_iter)
 
+    # create an output list for all steps to be added to
     all_data = []
+
+    # add the first row to the list
     all_data.append(first_step)
 
     for line in data_iter:
+        # read the next row in the iterator and get the start and end times for the next step
         next_time_off = datetime.datetime.strptime(line[0] + " " + line[1], "%Y-%m-%d %H:%M:%S")
         next_time_on = datetime.datetime.strptime(line[0] + " " + line[2], "%Y-%m-%d %H:%M:%S")
 
+        # assign these times to the next on and off steps
         previous_step = [previous_time_on, next_time_off, 'on']
         next_step = [next_time_off, next_time_on, 'off']
 
@@ -55,14 +61,15 @@ def process_file(input_file):
         if next_time_off > next_time_on:
             raise ValueError('Start time of off row cannot be after end time:', next_step)
 
+        # append these steps to the output list
         print(previous_step)
         all_data.append(previous_step)
 
         print(next_step)
         all_data.append(next_step)
 
+        # create the set-up for reading the next row, by assigning the end time from the line just read as the previous time for the next step
         previous_time_on = next_time_on
-
 
     return all_data
 
