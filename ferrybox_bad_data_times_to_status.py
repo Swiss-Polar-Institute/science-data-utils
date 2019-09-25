92486f9a5dc569a7c7beeed1057b7eb5888f9f3a
Jen Thomas <jenny_t152@yahoo.co.uk>
2019-09-25 09:36:17 +0100

    Added documentation to ferrybox code.

--- ferrybox_bad_data_times_to_status.py
+++ ferrybox_bad_data_times_to_status.py
@@ -1,40 +1,8 @@
 import csv
 import datetime
 
-
-def correct_seconds_same_minute(pump_log):
-    output = []
-
-    pending_add_second = 0
-    for row in pump_log:
-        if row[0].second == 0 and row[0] == row[1]:
-            row[1] = row[1] + datetime.timedelta(seconds=59)
-            output.append([row[0], row[1]])
-            # output.append([row[0] + datetime.timedelta(seconds=pending_add_second), row[1]])
-            pending_add_second = 1
-        else:
-            output.append([row[0], row[1]])
-            # output.append([row[0] + datetime.timedelta(seconds=pending_add_second), row[1]])
-            pending_add_second = 0
-
-    return output
-
-
-def process_file2(input_file):
-    pump_log = read_file_to_list(input_file)
-
-    pump_log = change_format_from_input_to_datetime(pump_log)
-
-    pump_log = collapse_same_day_off(pump_log)
-
-    pump_log = correct_seconds_same_minute(pump_log)
-
-    result = process_to_on_off(pump_log)
-
-    return result
-
-
 def read_file_to_list(input_file):
+    """Read a csv file of off times in the format date, time, time to a list in the format date, time, time."""
     with open(input_file) as csvfile:
         csv_rows = csv.reader(csvfile)
 
@@ -45,54 +13,126 @@ def read_file_to_list(input_file):
     return data
 
 
-def collapse_same_day_off(l):
-    output = []
+def change_format_from_input_to_datetime(list_d_t_t):
+    """Changes format of input list containing off times, from a list of lists each containing a date, time, time, to a list of lists
+    containing a date time, date time, both in python datetime type.
+    Input: [['2016-12-24', '11:30:00', '12:30:00'],
+            ['2016-12-25', '23:45:00', '23:50:00']
+            ]
 
-    list_iter = iter(l)
-
-    previous = next(list_iter)
-
-    output.append(previous)
-
-    for row in list_iter:
-        skipped = False
-        while previous[1] + datetime.timedelta(seconds=1) == row[0]:
-            skipped = True
-            previous = row
-            row = next(list_iter)
-
-        if skipped:
-            output[-1][1] = previous[1]
-            output.append([row[0], row[1]])
-        else:
-            output.append([row[0], row[1]])
-
-        previous = row
-
-    return output
-
-
-def change_format_from_input_to_datetime(list):
+    Output: [[datetime.datetime(2016, 12, 24, 11, 30, 00), datetime.datetime(2016, 12, 24, 12, 30, 00)],
+            [datetime.datetime(2016, 12, 25, 23, 45, 00), datetime.datetime(2016, 12, 25, 23, 50, 00)]
+            ]
+    """
     data_output = []
 
-    for row in list:
+    for row in list_d_t_t:
         data_output.append([datetime.datetime.strptime(row[0] + " " + row[1], "%Y-%m-%d %H:%M:%S"),
                             datetime.datetime.strptime(row[0] + " " + row[2], "%Y-%m-%d %H:%M:%S")])
 
     return data_output
 
 
-def process_to_on_off(l):
+def collapse_same_day_off(list_dt_dt):
+    """Puts off rows where they pass over midnight, into one row. Input and output lists are both datetime, datetime in
+    python datetime type.
+    Input: [[datetime.datetime(2016, 12, 24, 11, 30, 00), datetime.datetime(2016, 12, 24, 12, 30, 00)],
+                    [datetime.datetime(2016, 12, 25, 20, 45, 00), datetime.datetime(2016, 12, 25, 23, 59, 59)],
+                    [datetime.datetime(2016, 12, 26, 00, 00, 00), datetime.datetime(2016, 12, 26, 14, 30, 00)],
+                    [datetime.datetime(2016, 12, 27, 13, 0, 00), datetime.datetime(2016, 12, 27, 15, 45, 00)]
+                    ]
+    
+    Output: [[datetime.datetime(2016, 12, 24, 11, 30, 00), datetime.datetime(2016, 12, 24, 12, 30, 00)],
+                    [datetime.datetime(2016, 12, 25, 20, 45, 00), datetime.datetime(2016, 12, 26, 14, 30, 00)],
+                    [datetime.datetime(2016, 12, 27, 13, 0, 00), datetime.datetime(2016, 12, 27, 15, 45, 00)]
+                    ]
+
+    """
+    collapsed_list = []
+
+    list_iter = iter(list_dt_dt)
+
+    # read the first row
+    previous = next(list_iter)
+
+    collapsed_list.append(previous)
+
+    count_midnight_rows_skipped = 0
+
+    for row in list_iter:
+        skipped = False
+        while previous[1] + datetime.timedelta(seconds=1) == row[0]: # compare the end of the previous row with the
+            # start of the current row and if they are one second apart, then enter the condition to allow the rows
+            # to be collapsed (note that this has not been tested where the rows do not go over midnight)
+            skipped = True
+            previous = row
+            row = next(list_iter)
+            count_midnight_rows_skipped += 1
+
+        if skipped:
+            collapsed_list[-1][1] = previous[1] # change the end time of the previous row to be the end time of the
+            # current row (this has already been appended to the list, so it needs to be changed there)
+            collapsed_list.append([row[0], row[1]])
+        else:
+            collapsed_list.append([row[0], row[1]])
+
+        previous = row
+
+    return collapsed_list
+
+
+def correct_off_seconds_same_minute(pump_log):
+    """If an off row has the same start and end times and these are the same minute, we are assuming that the pump was
+    off for the entire minute, rather than just the one second.
+
+    This function adds 59 seconds to the end time so that the off row can be a period of time.
+
+    Input: [[datetime.datetime(2016, 12, 24, 11, 30, 00), datetime.datetime(2016, 12, 24, 11, 30, 00)],
+            [datetime.datetime(2016, 12, 25, 20, 45, 00), datetime.datetime(2016, 12, 26, 20, 45, 00)]
+            ]
+
+    Output: [[datetime.datetime(2016, 12, 24, 11, 30, 00), datetime.datetime(2016, 12, 24, 11, 30, 59)],
+            [datetime.datetime(2016, 12, 25, 20, 45, 00), datetime.datetime(2016, 12, 26, 20, 45, 59)]
+            ]
+    """
+    output = []
+
+    for row in pump_log:
+        if row[0].second == 0 and row[0] == row[1]:
+            row[1] = row[1] + datetime.timedelta(seconds=59)
+            output.append([row[0], row[1]])
+        else:
+            output.append([row[0], row[1]])
+
+    return output
+
+
+def process_to_on_off(list_dt_dt):
+    """Convert the list of off times to a list containing the off and now on periods, which are deduced from the off
+    times. This should be the list after it has been collapsed. Output list will contain a status, on or off.
+
+    Input: [[datetime.datetime(2016, 12, 24, 11, 30, 00), datetime.datetime(2016, 12, 24, 12, 30, 00)],
+            [datetime.datetime(2016, 12, 25, 20, 45, 00), datetime.datetime(2016, 12, 26, 14, 30, 00)],
+            [datetime.datetime(2016, 12, 27, 13, 0, 00), datetime.datetime(2016, 12, 27, 15, 45, 00)]
+            ]
+
+    Output: [[datetime.datetime(2016, 12, 24, 11, 30, 00), datetime.datetime(2016, 12, 24, 12, 30, 00), 'off'],
+            [datetime.datetime(2016, 12, 24, 12, 30, 00), datetime.datetime(2016, 12, 25, 20, 45, 00), 'on'],
+            [datetime.datetime(2016, 12, 25, 20, 45, 00), datetime.datetime(2016, 12, 26, 14, 30, 00), 'off'],
+            [datetime.datetime(2016, 12, 26, 14, 30, 00), datetime.datetime(2016, 12, 27, 13, 0, 00), 'on'],
+            [datetime.datetime(2016, 12, 27, 13, 0, 00), datetime.datetime(2016, 12, 27, 15, 45, 00) 'off']
+            ]
+
+    """
     output_list = []
 
-    list_iter = iter(l)
+    list_iter = iter(list_dt_dt)
 
+    # put the first row in the output_list - this is always off
     first_row = next(list_iter)
 
     previous_time_off_start = first_row[0]
     previous_time_off_end = first_row[1]
-
-    count_midnight_rows_skipped = 0
 
     output_list.append([previous_time_off_start, previous_time_off_end, 'off'])
 
@@ -100,167 +140,50 @@ def process_to_on_off(l):
         current_time_off_start = row[0]
         current_time_off_end = row[1]
 
-        output_list.append([previous_time_off_end, current_time_off_start, 'on'])
-        output_list.append([current_time_off_start, current_time_off_end, 'off'])
+        output_list.append([previous_time_off_end, current_time_off_start, 'on']) # the times covered by the periods inbetween the input rows
+        output_list.append([current_time_off_start, current_time_off_end, 'off']) # the times covered by the periods of the input rows
 
         previous_time_off_end = current_time_off_end
 
     return output_list
 
 
-def process_file(input_file):
-    """Get the input csv file and convert the times from off and on, to start and end of on/off status.
-
-    :param input_file: file path and name of csv file
-    """
-
-    data = read_file_to_list(input_file)
-    data.sort()
-
-    list_1 = process_to_on_off(data)
-
-    all_data = [] # output list of data
-    count_duplicate_lines_skipped = 0 # lines that are skipped because they are duplicates in the original file
-    count_midnight_rows_skipped = 0 # lines that are skipped because they go over midnight
-
-    # get the first row of data and create the first "step" out of it
-    first_row = data[0]
-
-    first_time_off = datetime.datetime.strptime(first_row[0] + " " + first_row[1], "%Y-%m-%d %H:%M:%S")
-    first_time_on = datetime.datetime.strptime(first_row[0] + " " + first_row[2], "%Y-%m-%d %H:%M:%S") - datetime.timedelta(seconds=1) # subtract one second so that on and off time steps do not overlap
-    print(first_time_on)
-    # this first row has already been designated as off
-    first_step = [first_time_off, first_time_on, 'off']
-
-    # set the time the pump was on for the next row, as the time from the first row
-    next_time_on = first_time_on + datetime.timedelta(seconds=1) # add the second to get a time that does not overlap with the off row
-    previous_time_on = next_time_on
-
-    # add the first row to the list
-    all_data.append(first_step)
-    previous_line = [first_time_off, first_time_on]
-
-    # create an iterator to avoid having to read the first row of the list (this has already been used)
-    data_iter = iter(data)
-    next(data_iter) # skips the first line
-
-    for line in data_iter:
-        # read the next row in the iterator and get the start and end times for the next step
-        current_line = [datetime.datetime.strptime(line[0] + ' ' + line[1], '%Y-%m-%d %H:%M:%S'), datetime.datetime.strptime(line[0] + ' ' + line[2], '%Y-%m-%d %H:%M:%S')]
-        next_time_off = current_line[0]
-        next_time_on = current_line[1]
-
-        # skip the line if it was the same as the previous line (this will remove the duplicates that existed)
-        if current_line == previous_line:
-            previous_line = current_line
-            count_duplicate_lines_skipped += 1
-            continue
-
-        while previous_line[1] + datetime.timedelta(seconds=1) == current_line[0]:
-            previous_line = [previous_line[0], datetime.datetime.strptime(line[0] + ' ' + line[2], '%Y-%m-%d %H:%M:%S')]
-            line = next(data_iter)
-
-            current_line = [datetime.datetime.strptime(line[0] + ' ' + line[1], '%Y-%m-%d %H:%M:%S'),
-                        datetime.datetime.strptime(line[0] + ' ' + line[2], '%Y-%m-%d %H:%M:%S')]
-
-            count_midnight_rows_skipped += 1
-
-        off_starts = previous_line[0]
-        off_ends = previous_line[1]
-
-        on_starts = previous_line[1]
-        on_ends = current_line[0]
-
-        all_data.append([off_starts, off_ends, 'off'])
-        all_data.append([on_starts, on_ends, 'on'])
-
-
-        # all_data.append([previous_line[0], previous_line[1], 'off'])
-        # all_data.append([previous_line[1], current_line[0], 'on'])
-
-        previous_line = current_line
-
-        # Check that the times in the steps make sense
-        if previous_time_on > next_time_off:
-            raise ValueError('Start time of on row cannot be after the end time:')
-
-        if next_time_off > next_time_on:
-            raise ValueError('Start time of off row cannot be after end time:', next_step)
-
-        # check difference between start and end times for each step:
-        on_difference = next_time_off - previous_time_on
-        off_difference = next_time_on - next_time_off
-
-        if off_difference.total_seconds() == 0 and next_time_off.second == 0:
-            # where the off times are the same to the nearest minute
-            #print('Off times are the same to the nearest minute', off_difference)
-
-            next_time_on += datetime.timedelta(seconds=59)
-            previous_time_off = next_time_off - datetime.timedelta(seconds=1)
-            previous_step = [previous_time_on, previous_time_off, 'on']
-            next_step = [next_time_off, next_time_on, 'off']
-            # create the set-up for reading the next row, by assigning the end time from the line just read as the previous time for the next step
-            previous_time_on = next_time_on + datetime.timedelta(seconds=1)
-        elif off_difference.total_seconds() == 0 and next_time_off.second != 0:
-            # where the off times are the same to the nearest second
-            #print('Off times are the same to the nearest second', off_difference)
-
-            next_time_on += datetime.timedelta(seconds=1)
-            previous_time_off = next_time_off - datetime.timedelta(seconds=1)
-            previous_step = [previous_time_on, previous_time_off, 'on']
-            next_step = [next_time_off, next_time_on, 'off']
-            # create the set-up for reading the next row, by assigning the end time from the line just read as the previous time for the next step
-            previous_time_on = next_time_on + datetime.timedelta(seconds=1)
-        else:
-            # THIS IS THE ALTERNATIVE WAY OF REMOVING THE ROWS THAT RUN OVER MIDNIGHT
-            # if previous_line[1] + datetime.timedelta(seconds=1) == current_line[0]: #and (previous_line[1].time() == datetime.datetime.strptime('23:59:59', '%H:%M:%S').time()):
-            #     # if there are rows that have off periods split over more than one day, then combine them into the same row
-            #     print('There is a row that goes over midnight')
-            #     print('Last row in all_data list: ', all_data[-1])
-            #     all_data[-1][1] = current_line[1]
-            #     print('Last row in all_data list changed to: ', all_data[-1])
-            #     count_midnight_rows_skipped += 1
-            #     previous_line = current_line
-            #     print()
-            #     continue
-            # all other cases
-            previous_time_off = next_time_off - datetime.timedelta(seconds=1)
-            previous_step = [previous_time_on, previous_time_off, 'on']
-            next_step_time_on = next_time_on
-            next_time_on = next_time_on - datetime.timedelta(seconds=1)
-            next_step = [next_time_off, next_time_on, 'off']
-            # create the set-up for reading the next row, by assigning the end time from the line just read as the previous time for the next step
-            previous_time_on = next_step_time_on
-
-        all_data.append(previous_step)
-
-        all_data.append(next_step)
-        previous_line = current_line
-        print('---------------------------------------------------------------------------')
-
-    print('Number of duplicate lines skipped: ', count_duplicate_lines_skipped)
-    print('Number of midnight lines skipped: ', count_midnight_rows_skipped)
-
-    return all_data
-
-
-def list_to_csv(list, output_file):
-    """Output the list of data to a csv file"""
+def list_to_csv(list, output_file, header):
+    """Output the list of data to a csv file with a specified header."""
     with open(output_file, 'w', ) as csvFile:
         writer = csv.writer(csvFile)
-        writer.writerow(['start_datetime', 'end_datetime', 'status'])
+        writer.writerow(header)
         writer.writerows(list)
 
     csvFile.close()
+
+
+def process_file(input_file):
+    """Process the input csv file to produce an output list of time periods with a status of on or off. """
+    off_periods = read_file_to_list(input_file)
+
+    off_periods_datetime = change_format_from_input_to_datetime(off_periods)
+
+    off_periods_collapsed = collapse_same_day_off(off_periods_datetime)
+
+    off_periods_corrected_seconds = correct_off_seconds_same_minute(off_periods_collapsed)
+
+    on_off_periods = process_to_on_off(off_periods_corrected_seconds)
+
+    return on_off_periods
+
 
 def main():
     input_file = '/home/carles/pump.csv'
     output_file = '/home/carles/pump_output.csv'
 
-    status_data = process_file2(input_file)
-    print(status_data)
-    print('total length: ', len(status_data))
-    list_to_csv(status_data, output_file)
+    on_off_periods = process_file(input_file)
+
+    print(on_off_periods)
+    print('total length: ', len(on_off_periods))
+
+    header = ['start_datetime', 'end_datetime', 'status']
+    list_to_csv(on_off_periods, output_file, header)
 
 
 if __name__ == "__main__":
--- test/test_ferrybox_bad_data_times_to_status.py
+++ test/test_ferrybox_bad_data_times_to_status.py
@@ -57,21 +57,23 @@ class TestFerryboxBadDataTimesToStatus(unittest.TestCase):
     #
     #     self.assertListEqual(actual, expected)
 
-    # def test_combine_multiday_rows(self):
-    #     pump_log = [['2016-12-25', '23:45:00', '23:50:00'],
-    #                 ['2016-12-26', '06:10:00', '20:40:00'],
-    #                 ['2016-12-27', '19:00:00', '20:00:00'],
-    #                 ]
-    #     expected = [[text_to_dt('2016-12-25 23:45:00'), text_to_dt('2016-12-25 23:50:00'), 'off'],
-    #                 [text_to_dt('2016-12-25 23:50:00'), text_to_dt('2016-12-26 06:10:00'), 'on'],
-    #                 [text_to_dt('2016-12-26 06:10:00'), text_to_dt('2016-12-26 20:40:00'), 'off'],
-    #                 [text_to_dt('2016-12-26 20:40:00'), text_to_dt('2016-12-27 19:00:00'), 'on'],
-    #                 [text_to_dt('2016-12-27 19:00:00'), text_to_dt('2016-12-27 20:00:00'), 'off'],
-    #                 ]
-    #
-    #     actual = ferrybox_bad_data_times_to_status.process_to_on_off(pump_log)
-    #
-    #     self.assertListEqual(actual, expected)
+    def test_combine_multiday_rows(self):
+        pump_log = [['2016-12-25', '23:45:00', '23:50:00'],
+                    ['2016-12-26', '06:10:00', '20:40:00'],
+                    ['2016-12-27', '19:00:00', '20:00:00'],
+                    ]
+        expected = [[text_to_dt('2016-12-25 23:45:00'), text_to_dt('2016-12-25 23:50:00'), 'off'],
+                    [text_to_dt('2016-12-25 23:50:00'), text_to_dt('2016-12-26 06:10:00'), 'on'],
+                    [text_to_dt('2016-12-26 06:10:00'), text_to_dt('2016-12-26 20:40:00'), 'off'],
+                    [text_to_dt('2016-12-26 20:40:00'), text_to_dt('2016-12-27 19:00:00'), 'on'],
+                    [text_to_dt('2016-12-27 19:00:00'), text_to_dt('2016-12-27 20:00:00'), 'off'],
+                    ]
+
+        pump_log = ferrybox_bad_data_times_to_status.change_format_from_input_to_datetime(pump_log)
+
+        actual = ferrybox_bad_data_times_to_status.process_to_on_off(pump_log)
+
+        self.assertListEqual(actual, expected)
 
     def test_combine_multiday_rows_join(self):
         pump_log = [['2016-12-24', '11:30:00', '12:30:00'],
