6cc04c3a9caea5a3544529e11a510bfb694e731e
Jen Thomas <jenny_t152@yahoo.co.uk>
2019-11-14 06:52:55 +0000

    Amended file out header. Changed file paths.

--- get_gebco_depth_along_track.py
+++ get_gebco_depth_along_track.py
@@ -29,7 +29,8 @@ def process_list_of_shapefiles(shapefile_list, raster, header, csvfile):
 
 def main():
 
-    raster_joined = '/home/jen/projects/ace_data_management/external_data/map_bathymetry/gebco/GEBCO2014_geotiff_joined.tif'
+    #raster_joined = '/home/jen/projects/ace_data_management/external_data/map_bathymetry/gebco/GEBCO2014_geotiff_joined.tif'
+    raster_joined = '/home/jen/projects/ace_data_management/external_data/map_bathymetry/gebco/GEBCO_2019_12_Nov_2019_356b1e29d3e1/gebco_2019_joined.tif'
 
     shapefile_201612 = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_2016-12.shp'
     shapefile_201701 = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_2017-01.shp'
@@ -38,8 +39,8 @@ def main():
 
     shapefile_list = [shapefile_201612, shapefile_201701, shapefile_201702, shapefile_201703]
 
-    header = ['longitude', 'latitude', 'depth_m']
-    csvfile_out = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_with_depth.csv'
+    header = ['date_time', 'latitude', 'longitude', 'depth_m']
+    csvfile_out = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_with_depth_gebco2019.csv'
 
     with open(csvfile_out, 'w') as csvfile:
         process_list_of_shapefiles(shapefile_list, raster_joined, header, csvfile)
