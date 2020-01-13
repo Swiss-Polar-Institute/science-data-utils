5deb2ac1202faa3f2628d97c042f641fdc3f429d
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-01-13 11:40:53 +0000

    Created converter from ISO datetime to time since an initial start time, in seconds.

---
+++ convert_isodatetime_to_timesince.py
@@ -0,0 +1,17 @@
+import datetime
+
+def convert_isodatetime_to_timesince_seconds(initial, isodatetime):
+    """Convert a datetime in ISO8601 format YYYY-MM-DDTHH:MI:SS+00:00 where +00:00 is the timezone and needs to be
+    specified, to a time since a specified initial datetime given in the same format. Return the output in seconds."""
+
+    # Convert the initial and ISO datetimes to Python datetime formats (rather than strings)
+    initial_dt = datetime.datetime.strptime(initial, '%Y-%m-%dT%H:%M:%S+00:00')
+    isodatetime_dt = datetime.datetime.strptime(isodatetime, '%Y-%m-%dT%H:%M:%S+00:00')
+
+    # Calculate the time difference between the two datetimes
+    timedelta = isodatetime_dt - initial_dt
+
+    # Convert the time difference to seconds
+    timedelta_secs = timedelta.total_seconds()
+
+    return timedelta_secs
