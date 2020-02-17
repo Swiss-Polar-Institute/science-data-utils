ca01313e2bdf396ba10b20f618ae1bc1c7aef80f
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-02-17 14:24:20 +0000

    Creates a pandas dataframe or writes data to csv file

--- check_depth_along_cruise_track.py
+++ check_depth_along_cruise_track.py
@@ -3,7 +3,6 @@ import pandas
 
 
 def file_to_position_depths(file_path):
-
     result = {}
     with open(file_path) as csvfile:
         contents = csv.reader(csvfile)
@@ -33,7 +32,7 @@ def file_to_position_depths(file_path):
     return result
 
 
-def print_differences(file_path, position_depths):
+def yield_difference(file_path, position_depths):
     with open(file_path) as csvfile:
         contents = csv.reader(csvfile)
 
@@ -47,16 +46,53 @@ def print_differences(file_path, position_depths):
             lat_long = (float(lat), float(long))
             depth = float(depth)
 
-            other_file_depth = position_depths[lat_long]
-            print(date, depth, other_file_depth, depth-other_file_depth)
+            other_file_depth = position_depths.get(lat_long, None)
 
-def compare_depths(depth1_file, depth2_file):
+            if other_file_depth is not None:
+                depth_difference = depth - other_file_depth
+            else:
+                depth_difference = None
+
+            yield [date, lat_long[0], lat_long[1], depth, other_file_depth, depth_difference]
+            # print(date, depth, other_file_depth, depth-other_file_depth)
+
+
+def write_csv_large_differences(depth1_file, depth2_file, csv_outfile):
     position_depths = file_to_position_depths(depth1_file)
 
-    print_differences(depth2_file, position_depths)
+    differences = yield_difference(depth2_file, position_depths)
+
+    header = ['Date', 'Lat', 'Long', 'Depth1', 'Depth2', 'Depth difference']
+    csv_writer = csv.writer(csv_outfile)
+    csv_writer.writerow(header)
+
+    for difference in differences:
+        if difference[5] is not None:
+            if abs(difference[5]) > 100:
+                csv_writer.writerow(difference)
+
+
+def create_dataframe(depth1_file, depth2_file):
+    position_depths = file_to_position_depths(depth1_file)
+
+    differences = yield_difference(depth2_file, position_depths)
+    df = pandas.DataFrame(differences, columns=['Date', 'Lat', 'Long', 'Depth1', 'Depth2', 'Depth difference'])
+
+    print(df.head())
+    print('test')
+
+    # for diff_line in yield_difference(depth2_file, position_depths):
+    #     print(diff_line)
+
+    # print_differences(depth2_file, position_depths)
+
 
 if __name__ == '__main__':
     gebco_csv = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_with_depth_gebco2019.csv'
     rtopo204_csv = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_with_depth_rtopo2.0.4.csv'
 
-    compare_depths(gebco_csv, rtopo204_csv)
+    csv_outfile = '/home/jen/projects/ace_data_management/mapping/data/depth_differences_greater_than_100.csv'
+
+    with open(csv_outfile, 'w') as csvfile:
+        write_csv_large_differences(gebco_csv, rtopo204_csv, csvfile)
+    # create_dataframe(gebco_csv, rtopo204_csv)
