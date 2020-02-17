74d5b7831d4170d720e01fd72889347620dec37d
Jen Thomas <jenny_t152@yahoo.co.uk>
2020-02-17 12:06:24 +0000

    Modified gebco file back to what it was originally

---
+++ check_depth_along_cruise_track.py
@@ -0,0 +1,5 @@
+import csv
+
+def compare_depths(depth1, depth2):
+    gebco_csv = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_with_depth_gebco2019.csv'
+    rtopo204_csv = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_with_depth_rtopo2.0.4.csv'
--- get_gebco_depth_along_track.py
+++ get_gebco_depth_along_track.py
@@ -30,21 +30,18 @@ def process_list_of_shapefiles(shapefile_list, raster, header, csvfile):
 def main():
 
     #raster_joined = '/home/jen/projects/ace_data_management/external_data/map_bathymetry/gebco/GEBCO2014_geotiff_joined.tif'
-    #raster_joined = '/home/jen/projects/ace_data_management/external_data/map_bathymetry/gebco/GEBCO_2019_12_Nov_2019_356b1e29d3e1/gebco_2019_joined.tif'
-    raster_joined = '/home/jen/rtopo2.0.4_30sec_bedrock_topography'
+    raster_joined = '/home/jen/projects/ace_data_management/external_data/map_bathymetry/gebco/GEBCO_2019_12_Nov_2019_356b1e29d3e1/gebco_2019_joined.tif'
 
     shapefile_201612 = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_2016-12.shp'
-    #shapefile_201701 = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_2017-01.shp'
-    #shapefile_201702 = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_2017-02.shp'
-    #shapefile_201703 = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_2017-03.shp'
+    shapefile_201701 = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_2017-01.shp'
+    shapefile_201702 = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_2017-02.shp'
+    shapefile_201703 = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_2017-03.shp'
 
-    #shapefile_list = [shapefile_201612, shapefile_201701, shapefile_201702, shapefile_201703]
-    shapefile_list = [shapefile_201612]
+    shapefile_list = [shapefile_201612, shapefile_201701, shapefile_201702, shapefile_201703]
 
 
     header = ['date_time', 'latitude', 'longitude', 'depth_m']
-    #csvfile_out = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_with_depth_gebco2019.csv'
-    csvfile_out = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_with_depth_rtopo2.0.4_2016-12.csv'
+    csvfile_out = '/home/jen/projects/ace_data_management/mapping/data/ace_cruise_track_1sec_with_depth_gebco2019.csv'
 
     with open(csvfile_out, 'w') as csvfile:
         process_list_of_shapefiles(shapefile_list, raster_joined, header, csvfile)
